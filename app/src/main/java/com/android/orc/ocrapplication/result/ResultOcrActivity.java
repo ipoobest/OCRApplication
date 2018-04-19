c6163d702c7948125e9b9592165b68331d1bf86e
hoikhong <poobest.pooh@gmail.com>
2018-04-19 12:51:31 +0700

    up botton toolbar ocrresult

--- app/src/main/java/com/android/orc/ocrapplication/result/ResultOcrActivity.java
+++ app/src/main/java/com/android/orc/ocrapplication/result/ResultOcrActivity.java
@@ -1,20 +1,32 @@
 package com.android.orc.ocrapplication.result;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.android.orc.ocrapplication.R;
 
 public class ResultOcrActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_ocr);
 
+        toolbar = findViewById(R.id.result_ocr_toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
+
         String request = getIntent().getStringExtra("stringRequest");
 
-        if (savedInstanceState == null){
+        if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.content_result_ocr, ResultOcrFragment.newInstance(request))
                     .commit();
