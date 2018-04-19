92bf3d016f7197a5c8c0b6f3fa4102e837985b82
hoikhong <poobest.pooh@gmail.com>
2018-04-19 13:40:22 +0700

    ResultListAdapter

--- app/src/main/java/com/android/orc/ocrapplication/adapter/ResultListAdapter.java
+++ app/src/main/java/com/android/orc/ocrapplication/adapter/ResultListAdapter.java
@@ -2,6 +2,7 @@ package com.android.orc.ocrapplication.adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import com.android.orc.ocrapplication.R;
 import com.android.orc.ocrapplication.callback.RecyclerViewClickListener;
 import com.android.orc.ocrapplication.dao.MenuDao;
 import com.android.orc.ocrapplication.holder.MenuItemHolder;
+import com.android.orc.ocrapplication.holder.MenuListHolder;
 import com.bumptech.glide.Glide;
 
 import java.util.List;
@@ -18,55 +20,21 @@ import java.util.List;
  * Created by j.poobest on 16/12/2017 AD.
  */
 
-public class ResultListAdapter {
-    Context context;
-    List<MenuDao> dao;
+public class ResultListAdapter extends RecyclerView.Adapter<MenuItemHolder> {
 
-    private RecyclerViewClickListener mListener;
-
-    public ResultListAdapter(Context context, RecyclerViewClickListener listener) {
-        this.context = context;
-        this.mListener = listener;
-    }
-
-    public void setDao(List<MenuDao> dao) {
-        this.dao = dao;
-    }
-
+    @NonNull
     @Override
-    public MenuItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.menu_item, parent, false);
-
-//        int height = parent.getMeasuredHeight() / 3;
-//        view.setMinimumHeight(height);
-
-        return new MenuItemHolder(view, mListener);
+    public MenuItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
     }
 
     @Override
     public void onBindViewHolder(@NonNull MenuItemHolder holder, int position) {
-        MenuDao item = dao.get(position);
-        holder.getNameMenu().setText(item.getName());
-
-        Glide.with(context)
-                .load(item.getImgUrl())
-                .into(holder.getImgMenu());
-
-
 
     }
 
     @Override
     public int getItemCount() {
-
-        if (dao == null) {
-            return 0;
-        }
-        if (dao.size() == 0) {
-            return 0;
-        }
-        return dao.size();
+        return 0;
     }
 }
