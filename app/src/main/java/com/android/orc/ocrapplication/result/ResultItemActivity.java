7504cf6823c4f06b7aaa1cfacb2c5f9c4ecfa8a7
hoikhong <poobest.pooh@gmail.com>
2018-04-23 17:03:12 +0700

    set up button Result ltem

--- app/src/main/java/com/android/orc/ocrapplication/result/ResultItemActivity.java
+++ app/src/main/java/com/android/orc/ocrapplication/result/ResultItemActivity.java
@@ -3,6 +3,7 @@ package com.android.orc.ocrapplication.result;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.android.orc.ocrapplication.R;
 import com.android.orc.ocrapplication.dao.MenuDao;
@@ -16,9 +17,10 @@ public class ResultItemActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_item);
-
         toolbar = findViewById(R.id.result_toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
         MenuDao dao = getIntent().getParcelableExtra("dao");
 
@@ -31,6 +33,12 @@ public class ResultItemActivity extends AppCompatActivity  {
         }
     }
 
-
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
